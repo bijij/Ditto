95be5045a1495510afe0a0bc5c34ae0b8ca1933b
Josh B <josh.ja.butt@gmail.com>
2019-10-08 19:27:38 +1000

    Clean up handling discord objects in config.

--- bot/config.py
+++ bot/config.py
@@ -9,9 +9,29 @@ import yaml
 _bot = None
 
 
+class HiddenRepr(str):
+    def __repr__(self):
+        return '<str with hidden value>'
+
+
+class Object(discord.Object):
+    def __init__(self, id, func):
+        self._func = func
+        super().__init__(id)
+
+    def __getattribute__(self, name):
+        if name is '_func':
+            return object.__getattribute__(self, name)
+
+        return getattr(self._func(), name, None)
+
+    def __repr__(self):
+        return getattr(self._func(), '__repr__', super().__repr__)()
+
+
 def _env_var_constructor(loader: yaml.Loader, node: yaml.Node):
     """Implements a custom YAML tag for loading optional environment variables.
-    If the environment variable is set it returns its value. 
+    If the environment variable is set it returns its value.
     Otherwise returns `None`.
 
     Example usage:
@@ -24,101 +44,16 @@ def _env_var_constructor(loader: yaml.Loader, node: yaml.Node):
     else:
         raise TypeError('Expected a string')
 
-    return os.getenv(key)
+    return HiddenRepr(os.getenv(key))
 
 
-def _guild_constructor(loader: yaml.Loader, node: yaml.Node):
-    """Implements a custom YAML tak for loading `discord.Guild` objects.
+def _generate_constructor(func):
 
-    Example usage:
-        key: !Guild guild_id
-    """
-    guild_id = int(loader.construct_scalar(node))
-    return lambda: _bot.get_guild(guild_id)
+    def constructor(loader: yaml.Loader, node: yaml.Node):
+        ids = [int(x) for x in loader.construct_scalar(node).split()]
+        return Object(ids[-1], lambda: func(*ids))
 
-
-def _channel_constructor(loader: yaml.Loader, node: yaml.Node):
-    """Implements a custom YAML tak for loading `discord.abc.GuildChannel` objects.
-
-    Example usage:
-        key: !Channel guild_id channel_id
-    """
-    guild_id, channel_id = [int(x)
-                            for x in loader.construct_scalar(node).split()]
-    return lambda: _bot.get_guild(guild_id).get_channel(channel_id)
-
-
-def _message_constructor(loader: yaml.Loader, node: yaml.Node):
-    """Implements a custom YAML tak for loading `discord.Message` objects.
-
-    Example usage:
-        key: !channel guild_id channel_id message_id
-
-    Note: This returns a coroutine which must be awaited.
-    """
-    guild_id, channel_id, message_id = [
-        int(x) for x in loader.constrcutc_scalar(node).split()]
-    return lambda: _bot.get_guild(guild_id).get_channel(channel_id).fetch_message(message_id)
-
-
-def _role_constructor(loader: yaml.Loader, node: yaml.Node):
-    """Implements a custom YAML tak for loading `discord.Role` objects.
-
-    Example usage:
-        key: !Role guild_id role_id
-    """
-    guild_id, role_id = [int(x)
-                         for x in loader.construct_scalar(node).split()]
-    return lambda: _bot.get_guild(guild_id).get_role(role_id)
-
-
-def _member_constructor(loader: yaml.Loader, node: yaml.Node):
-    """Implements a custom YAML tak for loading `discord.Member` objects.
-
-    Example usage:
-        key: !Member guild_id user_id
-    """
-    guild_id, member_id = [int(x)
-                           for x in loader.construct_scalar(node).split()]
-    return lambda: _bot.get_guild(guild_id).get_member(member_id)
-
-
-def _user_constructor(loader: yaml.Loader, node: yaml.Node):
-    """Implements a custom YAML tak for loading `discord.User` objects.
-
-    Example usage:
-        key: !User user_id
-    """
-    user_id = int(loader.construct_scalar(node))
-    return lambda: _bot.get_user(user_id)
-
-
-def _emoji_constructor(loader: yaml.Loader, node: yaml.Node):
-    """Implements a custom YAML tak for loading `discord.Emoji` objects.
-
-    Example usage:
-        key: !Emoji emoji_id
-    """
-    emoji_id = int(loader.construct_scalar(node))
-    return lambda: _bot.get_emoji(emoji_id)
-
-
-def _evaluate_lambda(value):
-    # Handle if object is list
-    if isinstance(value, list):
-        for index, item in enumerate(value):
-            value[index] = _evaluate_lambda(item)
-
-    # Handle if object is dict
-    if isinstance(value, dict):
-        for key, item in list(value.items()):
-            value[_evaluate_lambda(key)] = _evaluate_lambda(item)
-
-    # Handle if object is lambda function
-    if isinstance(value, LambdaType):
-        value = value()
-
-    return value
+    return constructor
 
 
 class Config(yaml.YAMLObject):
@@ -128,36 +63,33 @@ class Config(yaml.YAMLObject):
         for name, value in kwargs:
             setattr(self, name, value)
 
-    def __getattribute__(self, name):
-        value = object.__getattribute__(self, name)
-        return _evaluate_lambda(value)
-
     def __repr__(self):
         return f"<Config {' '.join(f'{key}={repr(value)}' for key, value in self.__dict__.items())}>"
 
 
-CONSTRUCTORS = [
-
-    # General constructors
-    ("Config", Config.from_yaml),
-    ("ENV", _env_var_constructor),
-
+DISCORD_CONSTRUCTORS = [
 
     # Discord constructors
-    ("Emoji", _emoji_constructor),
-    ("Guild", _guild_constructor),
-    ("User", _user_constructor),
+    ("Emoji", lambda e: _bot.get_emoji(e)),
+    ("Guild", lambda g: _bot.get_guild(g)),
+    ("User", lambda u: _bot.get_user(u)),
 
     # Discord Guild dependant constructors
-    ("Channel", _channel_constructor),
-    ("Member", _member_constructor),
-    ("Role", _role_constructor)
+    ("Channel", lambda g, c: _bot.get_guild(g).get_channel(c)),
+    ("Member", lambda g, m: _bot.get_guild(g).get_member(m)),
+    ("Role", lambda g, r: _bot.get_guild(g).get_role(r)),
 ]
 
-# Add constructors
-for key, constructor in CONSTRUCTORS:
-    yaml.FullLoader.add_constructor(f'!{key}', constructor)
 
-# Load the config
+# Add constructors
+yaml.FullLoader.add_constructor('!Config', Config.from_yaml)
+yaml.FullLoader.add_constructor('!ENV', _env_var_constructor)
+
+# Add discord specific constructors
+for key, func in DISCORD_CONSTRUCTORS:
+    yaml.FullLoader.add_constructor(
+        f'!{key}', _generate_constructor(func))
+
+    # Load the config
 with open("config.yml", encoding="UTF-8") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)  # type: Config
+    config: Config = yaml.load(f, Loader=yaml.FullLoader)
